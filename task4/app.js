1405179f0bb17a59209761e2a8c787dc15727fed
FENIX <fenix123fly@gmail.com>
2016-12-28 02:32:52 +0300

    refactoring (remove generators)

--- task4/app.js
+++ task4/app.js
@@ -1,15 +1,20 @@
 'use strict';
 
-const LinksInit = require('./app/Links');
+const Storage = require('./app/storage');
+const Parser = require('./app/parser');
+
 
 module.exports = {
 
-  parse (base_url, page) {
-    (new LinksInit(base_url, page))
+  parse(base_url, page, limit) {
+    console.time("Execution time took");
+    (new Parser(base_url, page, new Storage(limit)))
       .parse()
-      .then(links => {
-        console.log(links);
-        console.log(links.length);
+      .then(parser => {
+        console.log(parser);
+        console.timeEnd("Execution time took");
       })
+      .catch(err => console.log(err));
   }
+
 };
